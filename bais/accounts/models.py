954430950b9da4ce90f56710a110bdbc64e83e5d
mariamlsufi <mariamlsufi@gmail.com>
2023-08-13 10:34:02 -0500

    defaults for user fields

--- bais/accounts/models.py
+++ bais/accounts/models.py
@@ -11,13 +11,13 @@ class DeviceInfo(models.Model):
 class CustomUser(AbstractUser):
     # misc
     linked_device = models.ForeignKey(DeviceInfo, on_delete=models.SET_NULL, null=True)
-    app_greeting = models.CharField(max_length=50)
+    app_greeting = models.CharField(max_length=50, default='prioritize you <3')
 
     # color scheme
-    lightest = models.CharField(max_length = 7)
-    lighter = models.CharField(max_length = 7)
-    darker = models.CharField(max_length = 7)
-    darkest = models.CharField(max_length = 7)
+    lightest = models.CharField(max_length = 7, default='#8C8C8C')
+    lighter = models.CharField(max_length = 7, default='#626661')
+    darker = models.CharField(max_length = 7, default='#4E524D')
+    darkest = models.CharField(max_length = 7 default='#313630')
 
     def __str__(self):
         return self.username
