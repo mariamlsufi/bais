3ffdbdb786e995f728b8c52db222140ad67f19ef
mariamlsufi <mariamlsufi@gmail.com>
2023-08-14 14:21:21 -0500

    login functionality and pages

--- bais/bais/settings.py
+++ bais/bais/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bais',
     'accounts',
     'food',
     'reports',
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'bais.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        "DIRS": [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -131,3 +132,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "accounts.CustomUser" 
+
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
