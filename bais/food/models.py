8d2092e78f5139e76427da7d19a6129adf397b94
mariamlsufi <mariamlsufi@gmail.com>
2023-08-15 23:17:08 -0500

    initial food models completed

--- bais/food/models.py
+++ bais/food/models.py
@@ -1,3 +1,37 @@
-from django.db import models
-
 # Create your models here.
+from django.db import models
+from accounts.models import CustomUser
+from django.conf import settings
+
+class Meals(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date_added = models.DateTimeField(auto_now=True)
+    label = models.CharField(max_length = 50)
+    kcal = models.DecimalField(decimal_places=2)
+    g_protein = models.DecimalField(decimal_places=2)
+
+    protein = models.BooleanField()
+    fruit = models.BooleanField()
+    veg = models.BooleanField()
+    grains = models.BooleanField()
+    dairy = models.BooleanField()
+    fats = models.BooleanField()
+
+    photo =models.FileField(upload_to='uploads/')
+
+    def __str__(self):
+        return self.label
+    
+class CustomFood(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date_added = models.DateTimeField(auto_now=True)
+    label = models.CharField(max_length = 50)
+    kcal = models.DecimalField(decimal_places=2)
+    g_protein = models.DecimalField(decimal_places=2)
+
+
+class Foods(models.Model):
+    date_added = models.DateTimeField(auto_now=True)
+    label = models.CharField(max_length = 50)
+    kcal = models.DecimalField(decimal_places=2)
+    g_protein = models.DecimalField(decimal_places=2)
--- bais/workouts/models.py
+++ bais/workouts/models.py
@@ -1,3 +1,13 @@
-from django.db import models
 
+from django.db import models
+'''
 # Create your models here.
+# Create your models here.
+from django.db import models
+from django.conf import settings
+
+class Workouts(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    label = models.CharField
+
+'''
