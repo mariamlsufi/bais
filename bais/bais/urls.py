4b2d916f82a8be4978aca2260e06fe357beae820
mariamlsufi <mariamlsufi@gmail.com>
2023-08-19 07:47:27 -0600

    added base url mappings and tempaltes

--- bais/bais/urls.py
+++ bais/bais/urls.py
@@ -23,4 +23,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("workouts/", include("workouts.urls")),
+    path("food/", include("accounts.urls")),
+    path("reports/", include("reports.urls")),
 ]
