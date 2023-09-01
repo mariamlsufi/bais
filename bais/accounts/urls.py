cad56646a22ebb5e475019b94eea39b77530d828
mariamlsufi <mariamlsufi@gmail.com>
2023-08-31 20:41:26 -0600

    added accounts index page

--- bais/accounts/urls.py
+++ bais/accounts/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 
 from .views import SignUpView
+from django.views.generic.base import TemplateView
+
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
+    path('', TemplateView.as_view(template_name="accounts/index.html"), name='index'),
 ]
 
 '''
-path('', views.index, name='index'),
 path('link-device', views.link, name='link-device'),
 path('colors', views.colors, name='colors'),
 path('details', views.details, name='details'),
