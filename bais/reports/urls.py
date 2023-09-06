2acb2a985ce21856cd4980f00b7ed0f97ec67417
mariamlsufi <mariamlsufi@gmail.com>
2023-09-06 10:34:53 -0600

    added view function and html edits for report index page

--- bais/reports/urls.py
+++ bais/reports/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from django.views.generic.base import TemplateView
+from .views import index
 
-urlpatterns = []
+
+urlpatterns = [
+    #send report data to function as well
+    path('', index, name='index')
+]
 
 '''
-path('', views.index, name='index'),
 path('<int:pk>',views.report, name='report'),
 '''
--- bais/reports/views.py
+++ bais/reports/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+from .models import Report
+
 # Create your views here.
+def index(request):
+    # View code here..
+    reports = Report.objects.filter(user = request.user.id)
+    return render(
+        request,
+        "reports/index.html",
+        {
+            "reports" : reports,
+        }    
+)
