e7f0c88535e5d321c242c8335e1ba522e15f3ac8
mariamlsufi <mariamlsufi@gmail.com>
2023-10-09 13:23:19 -0600

    food progress page

--- bais/food/views.py
+++ bais/food/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponseRedirect, JsonResponse
 import csv
 from accounts.models import CustomUser
 from .models import CustomFood, Foods, Meals
-from datetime import datetime
+from datetime import datetime, timedelta
 from django.utils import timezone
 
 
@@ -117,6 +117,8 @@ def AllFoods(request):
     return JsonResponse({'custom_foods': food_list})
 
 def EditMeal(request, id):
+
+
     meal = get_object_or_404(Meals, id=id, user=request.user.id)
     
     if request.method == "POST":
@@ -139,3 +141,47 @@ def EditMeal(request, id):
 
 
     return render(request, "food/edit.html", {'meal': meal})
+
+def ProgressData(request):
+
+
+    user = get_object_or_404(CustomUser, id=request.user.id)
+    days = request.GET.get('days', 0)
+
+    progress_data = []
+
+    startdate = datetime.today()
+
+    for i in range(int(days)):
+        day_data = {
+            "kcal" : 0,
+            "g_protein" : 0,
+            "fruit" : 0,
+            "veg" : 0,
+            "grains" : 0,
+            "fats" : 0,
+            "dairy" : 0,
+            "protein" : 0,
+        }
+        # get meals for that day
+        curr_date = datetime.today() - timedelta(days=i)
+        meals = Meals.objects.filter(user=user, date_added__year=curr_date.year, 
+                         date_added__month=curr_date.month, 
+                         date_added__day=curr_date.day)
+        # for each meal
+        for meal in meals:
+            day_data["kcal"] += meal.kcal
+            day_data["g_protein"] += meal.g_protein
+            day_data["fruit"] += meal.fruit
+            day_data["veg"] += meal.veg
+            day_data["grains"] += meal.grains
+            day_data["fats"] += meal.fats
+            day_data["dairy"] += meal.dairy
+            day_data["protein"] += meal.protein
+
+        progress_data.append(day_data)
+
+    return JsonResponse({'data': progress_data})
+
+def Progress(request):
+    return render(request, "food/progress.html")
